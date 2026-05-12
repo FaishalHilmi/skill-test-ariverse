ee618274fcd9a70abf5c4a5423f02aa9be5df10f
Faisal Hilmi <faisalhilmi082@gmail.com>
2026-05-12 18:35:36 +0700

    feat: add years-options data

---
+++ data/years-options.ts
@@ -0,0 +1,14 @@
+export const YEAR_OPTIONS = [
+  {
+    label: "Semua Tahun",
+    value: "all",
+  },
+  {
+    label: "2024",
+    value: "2024",
+  },
+  {
+    label: "2023",
+    value: "2023",
+  },
+];
