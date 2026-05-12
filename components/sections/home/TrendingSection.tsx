c4e4dbc5e70413993453ea415d1f4b6f2853944a
Faisal Hilmi <faisalhilmi082@gmail.com>
2026-05-12 16:57:07 +0700

    feat: add trending section ui

---
+++ components/sections/home/TrendingSection.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { MoveRight } from "lucide-react";
+import { TrendingGameCardSkeleton } from "@/components/skeleton/card/TrendingGameCardSkeleton";
+import Link from "next/link";
+import games from "@/data/games.json";
+import TrendingGameCard from "@/components/game/card/TrendingGameCard";
+import useLoading from "@/hooks/useLoading";
+
+export default function TrendingSection() {
+  const isLoading = useLoading();
+
+  const featuredGames = games.filter((game) => game.featured == true).length;
+  const UnfeaturedGames = games.filter((game) => game.featured == false).length;
+
+  console.log(featuredGames);
+  console.log(UnfeaturedGames);
+
+  return (
+    <section className="py-12 md:py-20">
+      <div className="container mx-auto px-5 lg:px-16">
+        <div className="mb-8 flex items-center justify-between md:mb-12">
+          <div className="flex items-center gap-3">
+            <div className="h-1 w-12 rounded-full bg-primary" />
+
+            <h2 className="font-display text-3xl font-bold md:text-4xl">
+              Sedang Tren
+            </h2>
+          </div>
+
+          <Link
+            href="/games"
+            className="flex items-center gap-2 text-sm font-bold text-primary transition-all hover:gap-3"
+          >
+            Lihat Semua
+            <MoveRight className="h-4 w-4" />
+          </Link>
+        </div>
+
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-4">
+          {isLoading
+            ? Array.from({ length: 4 }).map((_, index) => (
+                <TrendingGameCardSkeleton key={index} />
+              ))
+            : games
+                .slice(0, 4)
+                .map((game) => <TrendingGameCard key={game.id} game={game} />)}
+        </div>
+      </div>
+    </section>
+  );
+}
