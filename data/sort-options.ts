9fd9a3963c1284a0ee38376f745b9b395baa4bbb
Faisal Hilmi <faisalhilmi082@gmail.com>
2026-05-12 18:35:23 +0700

    feat: add sort options data

---
+++ data/sort-options.ts
@@ -0,0 +1,18 @@
+export const SORT_OPTIONS = [
+  {
+    label: "Rating Tertinggi",
+    value: "rating",
+  },
+  {
+    label: "Terbaru",
+    value: "latest",
+  },
+  {
+    label: "Harga Terendah",
+    value: "price-low",
+  },
+  {
+    label: "Alphabetical",
+    value: "alphabetical",
+  },
+];
