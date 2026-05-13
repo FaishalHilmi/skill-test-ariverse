c95edf27b4cc4d358bfedeb5496043ebaf7c09f5
Faisal Hilmi <faisalhilmi082@gmail.com>
2026-05-13 22:17:07 +0700

    feat: add nav links data

---
+++ data/nav-links.ts
@@ -0,0 +1,14 @@
+export const NAVLINK = [
+    {
+      name: "Beranda",
+      href: "/",
+    },
+    {
+      name: "Daftar Game",
+      href: "/games",
+    },
+    {
+      name: "Wishlist",
+      href: "/wishlist",
+    },
+  ];
