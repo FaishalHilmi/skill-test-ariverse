43e536e12e4fe051d086025bd292a97dd743357c
Faisal Hilmi <faisalhilmi082@gmail.com>
2026-05-13 22:14:38 +0700

    fix: improve wishlist grid layout and skeleton loading

--- components/sections/whislist/GridSection.tsx
+++ components/sections/whislist/GridSection.tsx
@@ -13,13 +13,13 @@ export default function GridSection({ games }: { games: Game[] }) {
             flex gap-6 overflow-x-auto
             pb-8 scrollbar-hide
             md:grid md:grid-cols-2
-            md:gap-8 md:overflow-visible
-            lg:grid-cols-3
+            md:gap-6 md:overflow-visible
+            lg:grid-cols-4
           "
         >
           {isLoading
             ? Array.from({
-                length: games.length,
+                length: 6,
               }).map((_, index) => <WishlistGameCardSkeleton key={index} />)
             : games.map((game) => (
                 <WishlistGameCard key={game.id} game={game} />
