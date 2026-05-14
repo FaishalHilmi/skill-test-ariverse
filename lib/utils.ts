b466ce3de7cca64491f228c507c7d6cd82d89a43
Faisal Hilmi <faisalhilmi082@gmail.com>
2026-05-14 19:44:52 +0700

    feat: add date and rupiah formatter helper functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function formatDate(date: string) {
+  return new Date(date).toLocaleDateString("id-ID", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+}
+
+export function formatRupiah(price: number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    maximumFractionDigits: 0,
+  }).format(price);
+}
