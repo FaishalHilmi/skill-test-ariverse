533762d4b5706a3c2515bf29bd51e69cc2753378
Faisal Hilmi <faisalhilmi082@gmail.com>
2026-05-14 16:08:47 +0700

    feat: implement suspense loading for game list section

--- app/(catalog)/games/page.tsx
+++ app/(catalog)/games/page.tsx
@@ -1,9 +1,10 @@
 import GameListSection from "@/components/sections/games/GameListSection";
+import { Suspense } from "react";
 
 export default function GamePage() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <GameListSection />
-    </>
+    </Suspense>
   );
 }
